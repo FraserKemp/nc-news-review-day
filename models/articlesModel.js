fb93754b2a5bc0849dc40c84d0145ea4c83aa540
Fraser <fraser@Roberts-MacBook-Pro.local>
2019-05-14 16:33:23 +0100

    created and tested querys to order asc or desc, get articles by autor and topic

--- models/articlesModel.js
+++ models/articlesModel.js
@@ -1,11 +1,15 @@
 const connection = require('../db/connection');
 
-const fetchAllArticles = ({ sort_by = 'created_at' }) => {
+const fetchAllArticles = ({
+  sort_by = 'created_at',
+  order = 'desc',
+  author,
+  topic
+}) => {
   return connection
     .select(
       'articles.article_id',
       'articles.title',
-      'articles.body',
       'articles.votes',
       'articles.topic',
       'articles.author',
@@ -13,9 +17,13 @@ const fetchAllArticles = ({ sort_by = 'created_at' }) => {
     )
     .count('comments.article_id as count')
     .from('articles')
+    .modify(query => {
+      if (author) query.where('articles.author', author);
+      if (topic) query.where('articles.topic', topic);
+    })
     .leftJoin('comments', 'articles.article_id', '=', 'comments.article_id')
     .groupBy('articles.article_id')
-    .orderBy(sort_by)
+    .orderBy(sort_by, order)
     .then(articles => {
       console.log(articles);
       return articles;
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -45,7 +45,6 @@ describe.only('/', () => {
           expect(body.articles[0]).to.contain.keys(
             'article_id',
             'title',
-            'body',
             'votes',
             'topic',
             'author',
@@ -62,7 +61,54 @@ describe.only('/', () => {
         .get('/api/articles?sort_by=article_id')
         .expect(200)
         .then(({ body }) => {
-          expect(body.articles).to.be.sortedBy('article_id');
+          expect(body.articles).to.be.descendingBy('article_id');
+        });
+    });
+  });
+
+  describe('/api/articles?order=desc', () => {
+    it('GET status:200 - returns all the articles sorted by their default value in desc order', () => {
+      return request(app)
+        .get('/api/articles?order=desc')
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.articles).to.be.descendingBy('created_at');
+        });
+    });
+  });
+
+  describe('/api/articles?sort_by=article_id&order=asc', () => {
+    it('GET status:200 - returns all the articles sorted by their article_id in acending order', () => {
+      return request(app)
+        .get('/api/articles?sort_by=article_id&order=asc')
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.articles).to.be.ascendingBy('article_id');
+        });
+    });
+  });
+
+  describe('/api/articles?author=butter_bridge', () => {
+    it('GET status:200 - returns all the articles that belong to the author given in the query', () => {
+      return request(app)
+        .get('/api/articles?author=butter_bridge')
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.articles).to.have.lengthOf(3);
+        });
+    });
+  });
+
+  describe('/api/articles?topic=mitch', () => {
+    it('GET status:200 - returns all the topics that belong to that topic given in the query', () => {
+      return request(app)
+        .get('/api/articles?topic=mitch')
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.articles).to.have.lengthOf(11);
+          expect(body.articles[0].topic).to.eql('mitch');
+          expect(body.articles[4].topic).to.eql('mitch');
+          expect(body.articles[8].topic).to.eql('mitch');
         });
     });
   });
